168bbe8ddfe8ca4a1ebe36945af9d732d8449d72
Tharcila Contursi <tharcilakcontursi@gmail.com>
2024-03-05 14:08:05 +0100

    finalizando body e contador de data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 AOS.init();
+
+const dataDoEvento = new Date("Nov 10, 2024 17:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const constaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60
+
+   const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+   const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+   const minutosAteOevento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+   const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOevento}m ${segundosAteOEvento}s`;
+
+    if(diasAteOEvento < 0 ){
+        clearInterval(constaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado`;
+    }
+},1000)
